0820e10c5624965d4f42fa50038b5ae447e8eeab
Sailesh Shrestha <werewolf@NP-SSE-MBP-02.local>
2025-04-16 08:52:18 +0545

    Edited the homepage message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Slow down you crazy child!</h1>')
 })
 
 const PORT = 8080
